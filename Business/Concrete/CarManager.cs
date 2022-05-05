728a9e00b20a5ce98ab02762674e9cb217b88564
alihsynv <alihsynov@gmail.com>
2022-05-05 16:57:46 +0300

    05.05.2022

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,9 +18,46 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Name.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                if (car.Name.Length < 2)
+                {
+                    Console.WriteLine("Car name must be a minimum of 2 characters");
+                }
+                else
+                {
+                    Console.WriteLine("The car daily price must be greater than 0");
+                }
+
+            }
+        }
+
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
         }
+
+        public Car GetById(int id)
+        {
+            return _carDal.Get(c => c.Id == id);
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
     }
 }
