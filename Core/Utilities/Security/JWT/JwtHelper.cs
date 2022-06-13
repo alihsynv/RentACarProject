7d6c735bca96fa36e67513ea8255c4f273d007c2
alihsynv <alihsynov@gmail.com>
2022-06-13 15:51:34 +0400

    13.06.2022 SecurityKeyHelper

--- Core/Utilities/Security/JWT/JwtHelper.cs
+++ Core/Utilities/Security/JWT/JwtHelper.cs
@@ -1,4 +1,7 @@
 ﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encryption;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,17 @@ namespace Core.Utilities.Security.JWT
 {
     public class JwtHelper : ITokenHelper
     {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = (TokenOptions)Configuration.GetSection("TokenOptions");
+        }
+
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
         }
     }
 }
