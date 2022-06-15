3e318973a2148348123d5aacfd8018a31ea3ae51
alihsynv <alihsynov@gmail.com>
2022-06-15 15:55:18 +0400

    15.06.2022 Startup Configuration

--- Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolver/Autofac/AutofacBusinessModule.cs
@@ -5,6 +5,7 @@ using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Helpers.FileHelper;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -35,6 +36,11 @@ namespace Business.DependencyResolver.Autofac
             builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
 
             builder.RegisterType<FileHelperManager>().As<IFileHelper>().SingleInstance();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
+
+
 
 
 
